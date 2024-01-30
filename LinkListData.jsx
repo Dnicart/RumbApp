546f9062f0c22eac923794d928194b720363a7b6
Demetri <80382730+Dnicart@users.noreply.github.com>
2024-01-30 00:25:56 -0800

    Update LinkListData.jsx
    Uploaded the incorrect file data.

--- LinkListData.jsx
+++ LinkListData.jsx
@@ -1,87 +1,189 @@
-import React, { useEffect, useState } from "react";
-import debug from "sabio-debug";
-import externalLinksService from "../../services/externalLinksService";
-import LinkItem from "./LinkListData";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import "./stylelinklist.css";
-import { useNavigate } from "react-router-dom";
-import Table from "react-bootstrap/Table";
+import propTypes from "prop-types";
+import debug from "sabio-debug";
+import "./stylelinklistdata.css";
+import Modal from "react-bootstrap/Modal";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import ListLookUp from "./LinkListLookUp";
+import externalLinksService from "services/externalLinksService";
+import Swal from "sweetalert2";
+// import { tab } from "@testing-library/user-event/dist/types/convenience";
 
-const _logger = debug.extend("LinkList");
+const _logger = debug.extend("LinkListData");
 
-function LinkListTable() {
-  const [link, setLinks] = useState({
-    arrayOfLinks: [],
-    mappedLinks: [],
-  });
+function LinkListData(props) {
+  const [show, setShow] = useState(false);
 
-  useEffect(() => {
-    externalLinksService
-      .GetSelectByCreatedBy()
-      .then(onLinkSuccess)
-      .catch(onLinkError);
-  }, []);
+  const [linkData] = useState(props.link);
 
-  const onLinkSuccess = (response) => {
-    _logger("Array of links", response);
-    let linkArray = response.items;
+  const onClickUpdateSubmit = (values) => {
+    Swal.fire({
+      title: "Are you sure you want to edit this link?",
+      showCancelButton: true,
+      confirmButtonText: "Update",
+      denyButtonText: "Cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const payload = {
+          UrlTypeId: values.urlTypeId.id,
+          Url: values.url,
+          EntityId: values.entityId,
+          EntityTypeId: values.entityTypeId.id,
+        };
+        _logger("Form submitted with values", values);
+        externalLinksService
+          .updateExternalLinks(values.id, payload)
+          .then(onEditUpdateSuccess)
+          .catch(onUpdateError);
 
-    setLinks((prevState) => {
-      let newState = { ...prevState };
-      newState.arrayOfLinks = linkArray;
-      newState.mappedLinks = linkArray.map((link) => ({
-        ...link,
-      }));
-      return newState;
+        _logger("payload", payload);
+      } else if (result.isDenied) {
+        Swal.fire("Edit was not Saved", "", "info");
+      }
     });
   };
 
-  const onLinkError = (response) => {
-    _logger("There was an error.", response);
+  function onEditUpdateSuccess(response) {
+    Swal.fire("Updated", "The link has been updated.", "success");
+    _logger("Submission Successful", response);
+
+    //here you call that function you created that was passed as props
+  }
+
+  function onUpdateError(error) {
+    Swal.fire("Error!", "There was an issue editing the link", "error");
+    _logger("Link Form Error", error);
+  }
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleDelete = () => {
+    const id = props.link.id;
+    Swal.fire({
+      title: "Are you sure you want to delete this link?",
+      showCancelButton: true,
+      confirmButtonText: "Delete",
+      denyButtonText: "Cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        externalLinksService
+          .deleteExternalLinks(id)
+          .then(onDeleteSuccess)
+          .catch(onDeleteError);
+      } else if (result.isDenied) {
+        Swal.fire("Deletion is not saved", "", "info");
+      }
+    });
   };
 
-  const mapLinks = () => {
-    //debugger;
-    if (!link.mappedLinks.length) {
-      return (
-        <tr>
-          <td colSpan="3">No links available</td>
-        </tr>
-      );
-    } else {
-      return link.mappedLinks.map((link) => (
-        <LinkItem key={"listA-" + link.id} link={link} />
-      ));
-    }
+  const onDeleteSuccess = () => {
+    Swal.fire("Deleted!", "The link has been deleted.", "success");
   };
-  const links = mapLinks();
-  const navigate = useNavigate();
 
-  const goToNewLink = () => {
-    navigate(`/links/createnewlink`);
+  const onDeleteError = (error) => {
+    _logger("Error deleting link", error);
+    Swal.fire("Error!", "There was an issue deleting the link", "error");
   };
 
   return (
-    <React.Fragment>
-      <div>
-        <Button type="createNewLink" onClick={goToNewLink}>
-          Create new Link
+    <tr key={props.link.id}>
+      <td>{props.link.urlTypeId.name}</td>
+      <td>{props.link.url}</td>
+      <td>
+        <Button
+          variant="outline-primary"
+          className="edit-button"
+          onClick={handleShow}
+        >
+          Edit
         </Button>
-      </div>
-      <div className="link-list-table">
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Website</th>
-              <th>Url</th>
-              <th>Edit/Delete</th>
-            </tr>
-          </thead>
-          <tbody>{links}</tbody>
-        </Table>
-      </div>
-    </React.Fragment>
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header>
+            <Modal.Title>Edit Link</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Formik
+              initialValues={linkData}
+              onSubmit={onClickUpdateSubmit}
+              enableReinitialize={true}
+            >
+              <Form>
+                <label htmlFor="UrlType" className="form-label">
+                  <b>Url Type</b>
+                </label>
+                <Field
+                  as="select"
+                  name="urlTypeId"
+                  className="form-control form-field"
+                >
+                  <ListLookUp />
+                </Field>
+                <ErrorMessage
+                  name="urlTypeId"
+                  component="div"
+                  className="has-error"
+                />
+                <label htmlFor="Url" className="form-label">
+                  <b>Url</b>
+                </label>
+                <Field
+                  id="url"
+                  name="url"
+                  type="text"
+                  className="form-control form-field w-50"
+                />
+                <ErrorMessage
+                  name="urlText"
+                  component="div"
+                  className="has-error"
+                />
+                <div className="button-group">
+                  <Button
+                    className="button-group__close"
+                    variant="secondary"
+                    onClick={handleClose}
+                  >
+                    Close
+                  </Button>
+                  <Button
+                    className="button-group__sumbit"
+                    variant="primary"
+                    type="submit"
+                  >
+                    Save Changes
+                  </Button>
+                </div>
+              </Form>
+            </Formik>
+          </Modal.Body>
+        </Modal>
+        <Button
+          variant="outline-primary"
+          className="delete-button"
+          onClick={handleDelete}
+        >
+          Delete
+        </Button>
+      </td>
+    </tr>
   );
 }
 
-export default LinkListTable;
+LinkListData.propTypes = {
+  link: propTypes.shape({
+    id: propTypes.string.isRequired,
+    urlTypeId: propTypes.string.isRequired,
+    url: propTypes.string.isRequired,
+    name: propTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default LinkListData;
+1;
